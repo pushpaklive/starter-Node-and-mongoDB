59f608ad3f9b0638759481d08bd24810cd4e7452
Pushpak Choudhary <pushpaklive@gmail.com>
2018-05-24 15:39:56 +0530

    Adding file to connect node and mlab

---
+++ connectMongo.js
@@ -0,0 +1,25 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const MONGO_URL = 'mongodb://pushpak:pushpak@ds249575.mlab.com:49575/employeedb';
+
+MongoClient.connect(MONGO_URL, (err, db) => {  
+  if (err) {
+    return console.log(err);
+  }
+
+  // Do something with db here, like inserting a record
+  db.collection('notes').insertOne(
+    {
+      title: 'Hello MongoDB - from Pushpak',
+      text: 'Hopefully this works!'
+    },
+    function (err, res) {
+      if (err) {
+        db.close();
+        return console.log(err);
+      }
+      // Success
+      db.close();
+    }
+  )
+});
